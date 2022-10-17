ac4e0faea735fcff6e14b0d2ab7da574a159fd11
alijonoff <2110040@newuzbekistanuniversity.uz>
2022-10-17 15:35:48 +0500

    qw

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float min = 11.5F;
+        float sec =min*60;
+        System.out.println(sec);
     }
 }
